776fb3d53637bc7551b6d10f21223e6435887ce1
alinaherdean <44171230+alinaherdean@users.noreply.github.com>
2018-10-29 20:26:44 +0200

    add string variables

--- js/learn.js
+++ js/learn.js
@@ -1,1 +1,16 @@
 console.info(0);
+
+var nume = "Herdean";
+var prenume = "Alina";
+var owner = "Alina's";
+var msg1 = 'ai zis "Nik"?';
+var msg2 = 'Nu, am zis "Nicolae"';
+var template = `Text cu ghilimele " sau simple'`;
+var skills = ["html", "css", "js"];
+console.debug("auzi...? dar cum te cheama? -" + prenume);
+
+prenume = "Nick";
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("cum te cheama? -" + prenume);
